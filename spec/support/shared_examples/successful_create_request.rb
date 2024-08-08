1df8ac39e35e4e067a0e9b692aa7fe5c2ad1072c
Joaquin Garcia del Rio <jogarcia@itba.edu.ar>
2024-08-08 10:00:23 -0300

    fix: tests fixes

--- spec/support/shared_examples/successful_create_request.rb
+++ spec/support/shared_examples/successful_create_request.rb
@@ -1,6 +1,7 @@
 shared_examples 'successful create response' do
-  it 'creates the resource and persists it to the database' do
+  it 'creates the resource and persists it to the database without duplicates' do
     expect(created_resource).to be_present
+    expect(created_resource.count).to eq(1)
   end
 
   it 'responds with 201 status' do
