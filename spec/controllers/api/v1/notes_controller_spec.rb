136f51701b93350914c4a3e77c8b659e7fb7aae0
Joaquín García del Río <45571740+JoacoGDR@users.noreply.github.com>
2024-08-08 09:20:28 -0300

    feat: [TR-463] notes controller tests (#7)
    * feat: note model and migration
    
    Created Note table through a migration and its model
    
    * feat: user has_many notes
    
    Added the one to many relation to user model. Also fixed the order of fields in the note model.
    
    * fix: reverted changes to Gemfile.lock
    
    * fix: consistent validations and factory tests
    
    Defined Factory Bot model and added test to note_spec. Updated seed file to populate a few notes. Fixed data type of content in migration and relation consistencies with model.
    
    * fix: note_type enum
    
    Changed note_type to enum. Validated user_id presence too.
    
    * feat: note content_length validation
    
    * fix: i18n issues
    
    * fix: content length validation
    
    * fix: method validation names
    
    * fix: content_length method
    
    * fix: validate_content_word_count
    
    * fix: node model maintainability
    
    * fix: note model style
    
    * fix: linter correction
    
    * fix: note model maintainability
    
    * fix: utility thresholds
    
    * feat: initial node_spec
    
    * feat: long_threshold test
    
    * feat: note controller
    
    * feat: classify content tests
    
    * fix: style fixes
    
    * fix: note controller routes
    
    * fix: linter correction
    
    * feat: note controller authentication
    
    * feat: note controller tests
    
    * fix: note controller tests
    
    * fix: unnecessary tests
    
    * fix: note controller tests
    
    * fix: note controller tests
    
    Made tests more maintainable by not expecting a resulting string.
    
    * fix: note model scopes and exceptions
    
    * fix: linter correction
    
    * fix: note model tests utility sample
    
    * fix: note controller exceptions
    
    * fix: note controller style
    
    * fix: note controller
    
    * fix: note controller spec updated
    
    * fix: shared_example use
    
    * fix: note controller and exception handler
    
    * fix: note controller
    
    Generalized note_types validation by checking enum keys.
    
    * fix: note controller tests efficiency
    
    * fix: note controller tests expected attributes
    
    * fix: notes controller
    
    changed notes_filtered name
    
    * fix: changed tests
    
    * fix: tests cleanup
    
    * fix: required_params to valid_required_paramss
    
    * fix: tests cleanup

---
+++ spec/controllers/api/v1/notes_controller_spec.rb
@@ -0,0 +1,115 @@
+require 'rails_helper'
+
+describe Api::V1::NotesController, type: :controller do
+  describe 'GET #index' do
+    let(:user_note_count) { Faker::Number.between(from: 1, to: 10) }
+    let(:page_size) { Faker::Number.between(from: 1, to: user_note_count) }
+    let(:page) { Faker::Number.between(from: 1, to: (user_note_count / page_size)) }
+    let(:valid_required_params) do
+      {
+        note_type: Note.note_types.keys.sample,
+        page: page,
+        page_size: page_size,
+        order: %i[desc asc].sample
+      }
+    end
+
+    context 'when there is a user logged in' do
+      include_context 'with authenticated user'
+
+      context 'when fetching notes with all required params' do
+        let(:random_index) { Faker::Number.between(from: 0, to: response_body.size - 1) }
+        let(:expected_keys) { %w[id title note_type content_length] }
+        let(:filtered_notes) { Note.where(user_id: user.id, note_type: valid_required_params[:note_type]) }
+        let(:expected) do
+          filtered_notes.order(created_at: valid_required_params[:order])
+                        .with_pagination(valid_required_params[:page], valid_required_params[:page_size])
+        end
+
+        before do
+          create(:note, note_type: valid_required_params[:note_type])
+          create_list(:note, user_note_count, user: user, note_type: valid_required_params[:note_type])
+
+          get :index, params: valid_required_params
+        end
+
+        it 'responds with ordered array' do
+          expect(response_body[random_index]['id']).to eq(expected[random_index].id)
+        end
+
+        it_behaves_like 'paginated response'
+
+        it_behaves_like 'valid array response'
+      end
+
+      context 'when fetching notes with a missing required param' do
+        let(:missing_param)  { valid_required_params.keys.sample }
+
+        before { get :index, params: valid_required_params.except(missing_param) }
+
+        it_behaves_like 'missing parameter bad request'
+      end
+
+      context 'when fetching notes with an invalid note type' do
+        let(:invalid_note_type) { :invalid_type }
+
+        before { get :index, params: valid_required_params.merge(note_type: invalid_note_type) }
+
+        it_behaves_like 'unprocessable entity response'
+      end
+
+      context 'when fetching notes and there are none' do
+        before do
+          get :index, params: valid_required_params
+        end
+
+        it_behaves_like 'valid empty array response'
+      end
+    end
+
+    context 'when there is not a user logged in' do
+      context 'when fetching all the notes for user' do
+        before { get :index }
+
+        it_behaves_like 'unauthorized'
+      end
+    end
+  end
+
+  describe 'GET #show' do
+    context 'when there is a user logged in' do
+      include_context 'with authenticated user'
+
+      context 'when fetching a note' do
+        let(:note) { create(:note, user: user) }
+        let(:expected_keys) { %w[id title note_type word_count created_at content content_length user] }
+
+        before { get :show, params: { id: note.id } }
+
+        it_behaves_like 'valid object response'
+      end
+
+      context 'when fetching an invalid note' do
+        before { get :show, params: { id: Faker::Number.number } }
+
+        it_behaves_like 'not found response'
+      end
+
+      context 'when fetching another user\'s note' do
+        let(:other_user_note) { create(:note) }
+
+        before { get :show, params: { id: other_user_note.id } }
+
+        it_behaves_like 'not found response'
+      end
+    end
+
+    context 'when there is not a user logged in' do
+      context 'when fetching a note' do
+        before { get :show, params: { id: Faker::Number.number } }
+
+        it_behaves_like 'unauthorized'
+      end
+    end
+  end
+end
