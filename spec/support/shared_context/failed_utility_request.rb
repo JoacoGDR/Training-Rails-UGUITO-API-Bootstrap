b5af5a4961d7cba47797672764695cd132698500
Joaquin Garcia del Rio <jogarcia@itba.edu.ar>
2024-08-09 14:45:58 -0300

    fix: unnecessary file

--- spec/support/shared_context/failed_utility_request.rb
+++
@@ -1,9 +0,0 @@
-shared_context 'with failed utility request' do
-  before do
-    allow(utility_service_class).to receive(:new).and_return(utility_service_instance)
-    allow(utility_service_instance).to receive(utility_service_method)
-      .and_return(instance_double(utility_service_response_class,
-                                  code: expected_status_code, body: expected_response_body))
-    allow(utility_service_instance).to receive(:utility).and_return(utility)
-  end
-end
