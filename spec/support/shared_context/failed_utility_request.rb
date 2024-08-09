072a67dd21d9d16c80586acb6fedae441963705d
Joaquin Garcia del Rio <jogarcia@itba.edu.ar>
2024-08-09 14:40:08 -0300

    fix: cassette problem fixed

--- spec/support/shared_context/failed_utility_request.rb
+++ spec/support/shared_context/failed_utility_request.rb
@@ -1,12 +1,4 @@
 shared_context 'with failed utility request' do
-  subject(:execute_worker) do
-    described_class.new.execute(user.id, params)
-  end
-
-  let(:expected_status_code) { 500 }
-  let(:expected_response_body) { { error: 'message' } }
-  let(:utility_service_method) { :retrieve_notes }
-
   before do
     allow(utility_service_class).to receive(:new).and_return(utility_service_instance)
     allow(utility_service_instance).to receive(utility_service_method)
--- spec/workers/retrieve_notes_worker_spec.rb
+++ spec/workers/retrieve_notes_worker_spec.rb
@@ -8,14 +8,17 @@ describe RetrieveNotesWorker do
       end
     end
 
+    let(:author) { 'J.K. Rowling' }
+    let(:params) { { author: author } }
+    let(:user) { create(:user, utility: utility) }
+
     context 'with utility service' do
-      let(:author) { 'J.K. Rowling' }
-      let(:params) { { author: author } }
-      let(:user) { create(:user, utility: utility) }
-      let(:utilities) { %i[north_utility south_utility] }
+      let_it_be(:utilities) do
+        %i[north_utility south_utility]
+      end
 
       include_context 'with utility' do
-        let(:utility) { create(utilities.sample) }
+        let_it_be(:utility) { create(utilities.sample) }
       end
 
       context 'when the request to the utility succeeds' do
@@ -25,14 +28,25 @@ describe RetrieveNotesWorker do
           %i[title type created_at content user book]
         end
 
-        before { execute_worker }
-
         it_behaves_like 'valid worker array response'
       end
 
-      include_context 'with failed utility request' do
+      context 'when the request to the utility fails' do
+        subject(:execute_worker) do
+          described_class.new.execute(user.id, params)
+        end
 
-        before { execute_worker }
+        let(:expected_status_code) { 500 }
+        let(:expected_response_body) { { error: 'message' } }
+        let(:utility_service_method) { :retrieve_notes }
+
+        before do
+          allow(utility_service_class).to receive(:new).and_return(utility_service_instance)
+          allow(utility_service_instance).to receive(utility_service_method)
+            .and_return(instance_double(utility_service_response_class,
+                                        code: expected_status_code, body: expected_response_body))
+          allow(utility_service_instance).to receive(:utility).and_return(utility)
+        end
 
         it_behaves_like 'well formatted worker response'
       end
