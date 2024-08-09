1f4f7e546f8825a4c8c3a5d3f8b9875b8934c585
Joaquin Garcia del Rio <jogarcia@itba.edu.ar>
2024-08-09 17:32:03 -0300

    fix: test fixes

--- spec/support/shared_examples/valid_worker_array_response.rb
+++ spec/support/shared_examples/valid_worker_array_response.rb
@@ -1,13 +1,13 @@
 shared_examples 'valid worker array response' do
   it 'succeeds' do
-    expect(response_status).to eq 200
+    expect(execute_worker.first).to eq 200
   end
 
   it 'returns notes as array' do
-    expect(response_array).to be_instance_of(Array)
+    expect(execute_worker.second[root_key]).to be_instance_of(Array)
   end
 
   it 'returns the expected note keys' do
-    expect(response_array.first.keys).to contain_exactly(*expected_keys)
+    expect(execute_worker.second[root_key].first.keys).to contain_exactly(*expected_keys)
   end
 end
