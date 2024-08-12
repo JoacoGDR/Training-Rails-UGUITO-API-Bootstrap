736cdb4cbe7febba3f30e6704fddba116b190cf3
Joaquín García del Río <45571740+JoacoGDR@users.noreply.github.com>
2024-08-12 09:47:37 -0300

    feat: [TR-464] note create (#8)
    * feat: note model and migration
    
    Created Note table through a migration and its model
    
    * feat: user has_many notes
    
    Added the one to many relation to user model. Also fixed the order of fields in the note model.
    
    * fix: reverted changes to Gemfile.lock
    
    * fix: consistent validations and factory tests
    
    Defined Factory Bot model and added test to note_spec. Updated seed file to populate a few notes. Fixed data type of content in migration and relation consistencies with model.
    
    * fix: note_type enum
    
    Changed note_type to enum. Validated user_id presence too.
    
    * feat: note content_length validation
    
    * fix: i18n issues
    
    * fix: content length validation
    
    * fix: method validation names
    
    * fix: content_length method
    
    * fix: validate_content_word_count
    
    * fix: node model maintainability
    
    * fix: note model style
    
    * fix: linter correction
    
    * fix: note model maintainability
    
    * fix: utility thresholds
    
    * feat: initial node_spec
    
    * feat: long_threshold test
    
    * feat: note controller
    
    * feat: classify content tests
    
    * fix: style fixes
    
    * fix: note controller routes
    
    * fix: linter correction
    
    * feat: note controller authentication
    
    * feat: note controller tests
    
    * fix: note controller tests
    
    * fix: unnecessary tests
    
    * fix: note controller tests
    
    * feat: notes controller create
    
    * fix: note create response message
    
    * fix: note controller tests
    
    Made tests more maintainable by not expecting a resulting string.
    
    * fix: note model scopes and exceptions
    
    * fix: linter correction
    
    * fix: note model tests utility sample
    
    * fix: note controller exceptions
    
    * fix: note controller style
    
    * fix: note controller
    
    * fix: note controller spec updated
    
    * fix: note controller render create message
    
    * fix: shared_example use
    
    * fix: note controller and exception handler
    
    * fix: i18n
    
    * fix: note controller
    
    Generalized note_types validation by checking enum keys.
    
    * fix: note controller tests efficiency
    
    * fix: note controller tests expected attributes
    
    * fix: notes controller
    
    changed notes_filtered name
    
    * fix: changed tests
    
    * fix: error messages
    
    * fix: tests cleanup
    
    * fix: required_params to valid_required_paramss
    
    * fix: tests cleanup
    
    * fix: exception rescue and additional tests
    
    * fix: tests fixes
    
    * fix: test corrections
    
    * fix: rubocop

--- app/controllers/api/v1/notes_controller.rb
+++ app/controllers/api/v1/notes_controller.rb
@@ -2,11 +2,17 @@ module Api
   module V1
     class NotesController < ApplicationController
       before_action :authenticate_user!
+      rescue_from ActionController::ParameterMissing, with: :render_missing_parameter_error
+      rescue_from ActiveRecord::RecordInvalid, with: :note_create_validation_error
+
+      def create
+        return render_invalid_note_type if invalid_note_type?(create_note_parameters[:note_type])
+        create_note_and_render_success
+      end
 
       def index
-        return render_invalid_note_type if invalid_note_type?
+        return render_invalid_note_type if invalid_note_type?(params.require(:note_type))
         return render_invalid_order if invalid_order?
-
         render json: notes, status: :ok, each_serializer: BriefNoteSerializer
       end
 
@@ -16,6 +22,25 @@ module Api
 
       private
 
+      def create_note_and_render_success
+        create_note
+        render json: { message: I18n.t('success.messages.note.create_success') }, status: :created
+      end
+
+      def create_note
+        Note.create!(params.require(:note).permit(:title, :content, :note_type)
+                           .merge(user_id: current_user.id))
+      end
+
+      def create_note_parameters
+        require_nested(required_create_note_params, params)
+        params.require(:note).permit(:title, :content, :note_type)
+      end
+
+      def required_create_note_params
+        { note: { note_type: true, title: true, content: true } }
+      end
+
       def note
         current_user.notes.find(params.require(:id))
       end
@@ -24,8 +49,8 @@ module Api
         !%w[asc desc].include?(params.require(:order))
       end
 
-      def invalid_note_type?
-        Note.note_types.keys.exclude?(params.require(:note_type))
+      def invalid_note_type?(note_type)
+        Note.note_types.keys.exclude?(note_type)
       end
 
       def notes
@@ -43,6 +68,16 @@ module Api
 
       def render_invalid_order
         render_invalid_parameter_error(I18n.t('errors.messages.note.invalid_order'))
+      end
+
+      def render_missing_parameter_error
+        render json: { error: I18n.t('errors.messages.note.missing_param') }, status: :bad_request
+      end
+
+      def note_create_validation_error
+        threshold = current_user.utility.short_threshold
+        error_message = I18n.t('errors.messages.note.invalid_content_length', threshold: threshold)
+        render json: { error: error_message }, status: :unprocessable_entity
       end
     end
   end
