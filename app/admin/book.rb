1e777f6ccc30677059dc33d6e8f3e827bc32470a
Joaquín García del Río <45571740+JoacoGDR@users.noreply.github.com>
2024-08-12 09:59:07 -0300

    feat: [TR-469] admin book view (#10)
    * feat: note model and migration
    
    Created Note table through a migration and its model
    
    * feat: user has_many notes
    
    Added the one to many relation to user model. Also fixed the order of fields in the note model.
    
    * fix: reverted changes to Gemfile.lock
    
    * fix: consistent validations and factory tests
    
    Defined Factory Bot model and added test to note_spec. Updated seed file to populate a few notes. Fixed data type of content in migration and relation consistencies with model.
    
    * fix: note_type enum
    
    Changed note_type to enum. Validated user_id presence too.
    
    * feat: note content_length validation
    
    * fix: i18n issues
    
    * fix: content length validation
    
    * fix: method validation names
    
    * fix: content_length method
    
    * fix: validate_content_word_count
    
    * fix: node model maintainability
    
    * fix: note model style
    
    * fix: linter correction
    
    * fix: note model maintainability
    
    * fix: utility thresholds
    
    * feat: initial node_spec
    
    * feat: long_threshold test
    
    * feat: note controller
    
    * feat: classify content tests
    
    * fix: style fixes
    
    * fix: note controller routes
    
    * fix: linter correction
    
    * feat: note controller authentication
    
    * feat: note controller tests
    
    * fix: note controller tests
    
    * fix: unnecessary tests
    
    * fix: note controller tests
    
    * feat: notes controller create
    
    * fix: note create response message
    
    * fix: note controller tests
    
    Made tests more maintainable by not expecting a resulting string.
    
    * fix: note model scopes and exceptions
    
    * fix: linter correction
    
    * fix: note model tests utility sample
    
    * fix: note controller exceptions
    
    * fix: note controller style
    
    * fix: note controller
    
    * fix: note controller spec updated
    
    * fix: note controller render create message
    
    * fix: shared_example use
    
    * fix: note controller and exception handler
    
    * fix: i18n
    
    * fix: note controller
    
    Generalized note_types validation by checking enum keys.
    
    * fix: note controller tests efficiency
    
    * fix: note controller tests expected attributes
    
    * fix: notes controller
    
    changed notes_filtered name
    
    * fix: changed tests
    
    * fix: error messages
    
    * fix: tests cleanup
    
    * fix: required_params to valid_required_paramss
    
    * feat: index async
    
    * feat: index async #2
    
    * fix: tests cleanup
    
    * fix: exception rescue and additional tests
    
    * feat: admin book view
    
    * fix: show book html
    
    * fix: tests fixes
    
    * fix: store_accessor line style
    
    Co-authored-by: JoaquinHetrea <joahetrea@gmail.com>
    
    * fix: etiquette to string in mapper
    
    Co-authored-by: JoaquinHetrea <joahetrea@gmail.com>
    
    * fix: response_mapper styles
    
    * feat: one test added
    
    * feat: rest of tests added
    
    * fix: actions outside of form html
    
    Co-authored-by: JoaquinHetrea <joahetrea@gmail.com>
    
    * fix: show method deleted.
    
    * fix: linter fix
    
    * fix: annotation and permit_params redefinition
    
    * fix: mapper style
    
    * fix: cassette problem fixed
    
    * fix: unnecessary file
    
    * fix: test fixes
    
    * fix: full_name key of south utility service
    
    * fix: annotation deleted
    
    * fix: deleted custom view
    
    ---------
    
    Co-authored-by: JoaquinHetrea <joahetrea@gmail.com>

---
+++ app/admin/book.rb
@@ -0,0 +1,46 @@
+ActiveAdmin.register Book do
+  includes :utility, :user
+
+  filter :utility
+  filter :user, as: :select, collection: User.all.map { |user| [user.email, user.id] }
+  filter :genre
+  filter :author
+  filter :title
+  filter :publisher
+  filter :year
+
+  permit_params %i[utility_id user_id genre author image title publisher year]
+
+  member_action :copy, method: :get do
+    @book = resource.dup
+    render :new, layout: false
+  end
+
+  index do
+    selectable_column
+    id_column
+    column :utility
+    column :user
+    column :genre
+    column :author
+    column :title
+    column :publisher
+    column :year
+    actions
+  end
+
+  form do |f|
+    f.inputs 'Book Details', allow_destroy: true do
+      f.semantic_errors(*f.object.errors.keys)
+      f.input :utility
+      f.input :user, as: :select, collection: User.all.map { |user| [user.email, user.id] }
+      f.input :genre
+      f.input :author
+      f.input :image
+      f.input :title
+      f.input :publisher
+      f.input :year
+    end
+    f.actions
+  end
+end
